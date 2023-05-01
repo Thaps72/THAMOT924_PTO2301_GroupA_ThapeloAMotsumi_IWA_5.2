fad415fa6b947ec79bda126e4f7d0fb2b37dddb5
Thapelo A Motsumi <Thapelo.umuziphotoclub@gmail.com>
2023-05-01 21:19:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+/*=
+Free_WARNING = "Free shipping only applies to single customer orders"/ values are not declared and no semicolon at the end
+BANNED_WARNING = Unfortunately we do not ship to your country of residence"
+NONED_SELECTED = "0"/ Nunmbers must not have a coms on top
+
+if (location =RSA ) { shipping === 400 && currency === "R"}
+if location =NAM
+shipping = 600
+else shipping = 800
+
+shoes = 300 * 1
+toys - 100 * 5
+shirts = 150 "NONE_SELECTED"
+pens = 5 * "NONE_SELECTED"
+
+shipping = null
+currency = $
+
+if(shoes + batteries + pens + shirts > 1000 && ){
+  if (location = NAM && customers < 2){
+    if (location= RSA)
+    shipping =0 // calcShipping
+  }
+ }
+}
+
+if (shipping = 0 ) && ( customers !===1){
+  console.log(WARNING) }
+
+  location = 'NK' ? console.log(WARNING): console.log ( "price", currency, shoes + batteries + pens + shirts + shipping)
+
+customers = "1"
+const location = "RSA"
+currency = null 
+=*/
+
+/*my possible solution*/
+
+const Free_WARNING = "Free shipping only applies to single customer orders"; 
+const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
+const NONED_SELECTED = "0";
